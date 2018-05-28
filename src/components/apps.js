7fd6fe0869294d011355993f476fbe2938e8cdc2
Andreas Dewes <andreas.dewes@7scientists.com>
2018-05-28 15:05:30 +0200

    Also show opt-out / required status in list.

--- src/components/apps.js
+++ src/components/apps.js
@@ -10,10 +10,13 @@ export default class Apps extends React.Component {
                 toggle(app, value)
             }
             const required = app.required || config.required || false
+            const optOut = app.optOut || config.optOut || false
             const checked = manager.getConsent(app.name)
+            const optOutText = optOut ? <span class="cm-opt-out" title={t(['app', 'optOut', 'description'])}>{t(['app', 'optOut', 'title'])}</span> : ''
+            const requiredText = required ? <span class="cm-required" title={t(['app', 'required', 'description'])}>{t(['app', 'required', 'title'])}</span> : ''
             return <li>
                 <Switch disabled={required} checked={checked || required} onToggle={toggleApp} />
-                <span><b>{app.title}{required ? ' ' + t(['app', 'required']) : ''}</b></span>
+                <span><b>{app.title}{requiredText}{optOutText}</b></span>
                 <p>{t([app.name, 'description'])}</p>
             </li>
         })
