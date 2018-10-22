61b740897b3ebf61ad9e135febd2724bf0acf385
Emmanuel Pelletier <e.pelletier@vtech.fr>
2018-10-22 12:14:47 +0200

    adding default apps config easy to use in a project
    this is made when working with klaro as a dependency when managing
    packages via npm

---
+++ src/apps/google-analytics.js
@@ -0,0 +1,6 @@
+export default {
+	name: 'google-analytics',
+	title : 'Google Analytics',
+	purposes : ['analytics'],
+	cookies: ['_ga', '_gat', '_gid', '__utma', '__utmb', '__utmc', '__utmt', '__utmz'],
+};
---
+++ src/apps/google-tag-manager.js
@@ -0,0 +1,6 @@
+ export default {
+	name: 'google-tag-manager',
+	title : 'Google Tag Manager',
+	purposes : ['analytics'],
+	cookies: ['_ga', '_gat', '_gid', '__utma', '__utmb', '__utmc', '__utmt', '__utmz'],
+};
