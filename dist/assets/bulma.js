125e7ec3ede45da2b0067455aba9876820bb7580
mkkeck <github@michaelkeck.de>
2019-11-21 11:36:56 +0100

    Fixes for older browsers: IE 9 + is supported now.

--- dist/assets/bulma.js
+++ dist/assets/bulma.js
@@ -2,9 +2,11 @@
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
     var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
+    if (burger) {
+        var menu = document.querySelector('#' + burger.getAttribute('data-target'));
+        burger.addEventListener('click', function () {
+            burger.classList.toggle('is-active');
+            menu.classList.toggle('is-active');
+        });
+    }
 })();
