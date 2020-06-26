8e14493dc49f8fbc029dab064a36ffb3047cc50c
Marcel Tams <17073000+amtee@users.noreply.github.com>
2020-06-26 18:38:51 +0200

    Task: add mdn resource

--- dist/config.js
+++ dist/config.js
@@ -221,7 +221,8 @@ var klaroConfig = {
                 // properly, the cookie can't be deleted by Klaro
                 // (there is no way to access the path or domain of a cookie in JS)
                 // Notice that it is not possible to delete cookies that were set
-                // on a third-party domain!
+                // on a third-party domain! See the note at mdn:
+                // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#new-cookie_domain
                 [/^_pk_.*$/, '/', 'klaro.kiprotect.com'], //for the production version
                 [/^_pk_.*$/, '/', 'localhost'], //for the local version
                 'piwik_ignore',
