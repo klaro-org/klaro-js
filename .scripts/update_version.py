40a96d55ca4a62db5ee99085516ac26564f469fb
Andreas Dewes <andreas.dewes@kiprotect.com>
2020-07-29 14:42:58 +0200

    Worked on release mechanism.

---
+++ .scripts/update_version.py
@@ -0,0 +1,36 @@
+import re
+import os
+import sys
+import json
+import subprocess
+
+package_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "package.json")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        rt = 'patch'
+    else:
+        rt = sys.argv[1]
+    if not rt in ('patch', 'minor', 'major'):
+        sys.stderr.write("usage: {} patch|minor|major".format(sys.argv[0]))
+        exit(1)
+    with open(package_path) as input_file:
+        config = json.load(input_file)
+    match = re.match(r"^(\d+)\.(\d+)\.(\d+)$", config['version'])
+    if not match:
+        sys.stderr.write("version {} does not match!".format(config['version']))
+        exit(1)
+    patch_version, minor_version, major_version = int(match.group(3)), int(match.group(2)), int(match.group(1))
+    if rt == 'patch':
+        patch_version += 1
+    elif rt == 'minor':
+        minor_version += 1
+    elif rt == 'major':
+        major_version += 1
+    v = f"{major_version}.{minor_version}.{patch_version}"
+    config['version'] = v
+    with open(package_path, 'w') as output_file:
+        json.dump(config, output_file, indent=2, sort_keys=True)
+    subprocess.check_output(["git", "add", "."])
+    subprocess.check_output(["git", "commit", "-m", f"v{v}"])
+    subprocess.check_output(["git", "tag", "-a", f"v{v}", "-m", f"v{v}"])
--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 build: klaro klaro-no-css
 
+RT=patch
+
 release:
-	@git  diff --quiet || (echo "working directory not clean" && exit 1)
-	make build
-	sed -i 's/"version":\s*".*"/\"version\": "${VERSION}"/' package.json
-	git add .
-	git commit -m "v${VERSION}"
-	git tag -a v${VERSION} -m v${VERSION}
+	@echo "Making a '${RT}' release (change by setting RELEASE=patch|minor|major)"
+	# @git  diff --quiet || (echo "working directory not clean" && exit 1)
+	#make build
+	VERSION=$(python .scripts/update_version.py ${RT}); \
+	echo  ${VERSION}
+	exit  0
+	#git add .
+	#git commit -m "v${VERSION}"
+	#git tag -a v${VERSION} -m v${VERSION}
 
 klaro:
 	npm run-script make
