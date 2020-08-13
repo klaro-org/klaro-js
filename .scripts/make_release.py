0f7f4fa7625695a3e0daa1ce8d3ccf44eeea2b51
Andreas Dewes <andreas.dewes@kiprotect.com>
2020-08-13 23:52:56 +0200

    Add hashsums to release files.

--- .scripts/make_release.py
+++ .scripts/make_release.py
@@ -3,12 +3,34 @@ import os
 import sys
 import json
 import yaml
+import hashlib
 import subprocess
 
 wd = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 package_path = os.path.join(wd, "package.json")
 releases_yml_path = os.path.join(wd, "releases.yml")
 releases_json_path = os.path.join(wd, "releases.json")
+
+build_path = os.path.join(wd, "dist")
+release_files = ["klaro.js", "klaro-no-css.js", "klaro.css", "klaro.min.css"]
+
+# this forces pyyaml to produce cleaner looking strings
+# https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+def str_presenter(dumper, data):
+  if len(data.splitlines()) > 1:  # check for multiline string
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+  return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+yaml.add_representer(str, str_presenter)
+
+def hash_file(filename, methods=[hashlib.sha256]):
+    d = {}
+    with open(filename, "rb") as file:
+        c = file.read()
+        for method in methods:
+            # a bit hacky but it works...
+            d[method.__name__.split("_")[1]] = method(c).hexdigest()
+    return d
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -41,21 +63,41 @@ if __name__ == '__main__':
         if release["version"] == v:
             if (release.get("changelog") and release.get("date") and
                 re.match(r'^\d{4}\-\d{2}\-\d{2}$', release["date"]) and
-                release.get("tags") and isinstance(release["tags"], list)):
+                release.get("tags") is not None and isinstance(release["tags"], (list, tuple))):
                 break
     else:
         sys.stderr.write(f"please add a proper entry for version '{v}'in the releases YAML file (releases.yml)\n")
         exit(-1)
-    config['version'] = v
-    # we update the package file
-    with open(package_path, 'w') as output_file:
-        json.dump(config, output_file, indent=2, sort_keys=True)
+
+    env = os.environ.copy()
+    env['APP_VERSION'] = v
+    #subprocess.check_output(["make", "build"], cwd=wd, env=env)
+
+    # we add the SHA sums of the files to the release (can be used for
+    # subresource integrity)
+    files = []
+    for filename in release_files:
+        d = {
+            "name" : filename,
+        }
+        d.update(hash_file(os.path.join(build_path, filename),
+            methods=[hashlib.sha256, hashlib.sha1, hashlib.md5, hashlib.sha384]))
+        files.append(d)
+    release["files"] = files
+
+    with open(releases_yml_path, 'w') as output_file:
+        output_file.write(yaml.dump(releases, indent=2, sort_keys=True, default_flow_style=False))
+
     # we update the JSON releases file to reflect the YAML one
     with open(releases_json_path, 'w') as output_file:
         json.dump(releases, output_file, indent=2, sort_keys=True)
-    env = os.environ.copy()
-    env['APP_VERSION'] = v
-    subprocess.check_output(["make", "build"], cwd=wd, env=env)
+
+    # we update the package file
+    config['version'] = v
+    with open(package_path, 'w') as output_file:
+        json.dump(config, output_file, indent=2, sort_keys=True)
+
+
     subprocess.check_output(["git", "add", "."], cwd=wd)
     subprocess.check_output(["git", "commit", "-m", f"v{v}"], cwd=wd)
     subprocess.check_output(["git", "tag", "-a", f"v{v}", "-m", f"v{v}"], cwd=wd)
--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ RT=patch
 
 release:
 	@echo "Making a '${RT}' release (change by setting RT=patch|minor|major)"
-	@git  diff --quiet || (echo "working directory not clean" && exit 1)
+	@git  diff --quiet src || (echo "source directory not clean" && exit 1)
 	@npm run-script lint-fix || (echo "fix your errors first" && exit 1)
 	python3 .scripts/make_release.py ${RT}
 
@@ -13,6 +13,7 @@ klaro:
 
 klaro-no-css:
 	SEPARATE_CSS=1 npm run-script make
+	SEPARATE_CSS=1 NO_MINIFY_CSS=1 npm run-script make
 
 publish: build
 	npm publish
