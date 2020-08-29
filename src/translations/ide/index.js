f25a0b7c3a979d02eea6597b84771c4cf3e2fbf6
Andreas Dewes <andreas.dewes@kiprotect.com>
2020-08-29 17:59:01 +0200

    Add separate translations path for IDE.

---
+++ src/translations/ide/index.js
@@ -0,0 +1,26 @@
+// To create a new translation, add a YAML file with the required entries and
+// link it here using the appropriate language code.
+
+import zh from './zh.yml'
+import pt from './pt.yml'
+import de from './de.yml'
+import en from './en.yml'
+import es from './es.yml'
+import fr from './fr.yml'
+import it from './it.yml'
+import nl from './nl.yml'
+import no from './no.yml'
+import pl from './pl.yml'
+
+export default {
+    zh,
+    pz,
+    de,
+    en,
+    es,
+    fr,
+    it,
+    nl,
+    no,
+    pl,
+}
